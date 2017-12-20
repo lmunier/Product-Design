2c5e4b9bf522c741ec574a4570dcd1476e5d1319
theosche <34695683+theosche@users.noreply.github.com>
2017-12-20 23:00:43 +0100

    Add files via upload

---
+++ essai.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SmartBin_zip</title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<script type="text/javascript" src="ExternalJS.js"></script>	<!-- CSS et Javascript externes -->
+</head>
+<body>
+		<ul class= "drop-down">
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+		</ul>
+		<?php
+			echo("Hello");
+		?>
+		<ul class= "drop-down">
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+		</ul>
+		<ul class= "drop-down">
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+		</ul>
+		<ul class= "drop-down">
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+			<li class="list"><div class = "element">CO015 Poubelle 2</div><div class = "element">2017-12-19: 20h43</div><div class = "element">55%</div><div class = "element">55%</div></li>
+		</ul>
+</body>
+</html>
